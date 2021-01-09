c3490937e0306729b3c22814a33f431d3f6cf18a
Zac <azac281@gmail.com>
2021-01-08 21:44:42 -0600

    Added drag and drop for color pallet

--- src/components/ColorPallet/ColorPallet.js
+++ src/components/ColorPallet/ColorPallet.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import Color from "../Color/Color";
 import ColorPalletHeader from "./ColorPalletHeader";
 function ColorPallet({
@@ -7,6 +8,15 @@ function ColorPallet({
   palletName,
   setPalletName,
 }) {
+  const handleOnDragEnd = (result) => {
+    const newItems = [...colorPallet];
+    const draggedItem = newItems[result.source.index];
+    newItems.splice(result.source.index, 1);
+    newItems.splice(result.destination.index, 0, draggedItem);
+    console.log(newItems);
+    setColorPallet(newItems);
+  };
+
   return (
     <section className="color-pallet">
       <ColorPalletHeader
@@ -14,18 +24,42 @@ function ColorPallet({
         setPalletName={setPalletName}
       />
       <div className="color">
-        {colorPallet.map((color, index) => {
-          return (
-            <Color
-              key={index}
-              {...color}
-              index={index}
-              hexColor={color.hexColor}
-              setColorPallet={setColorPallet}
-              colorPallet={colorPallet}
-            />
-          );
-        })}
+        <DragDropContext onDragEnd={handleOnDragEnd}>
+          <Droppable droppableId="colorPallet">
+            {(provided) => (
+              <div {...provided.droppableProps} ref={provided.innerRef}>
+                {colorPallet.map((color, index) => {
+                  return (
+                    <Draggable
+                      key={color.index.toString()}
+                      index={index}
+                      draggableId={color.index.toString()}
+                    >
+                      {(provided) => (
+                        <div
+                          id={color.index.toString()}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                          ref={provided.innerRef}
+                        >
+                          <Color
+                            key={index}
+                            {...color}
+                            index={index}
+                            hexColor={color.hexColor}
+                            setColorPallet={setColorPallet}
+                            colorPallet={colorPallet}
+                          />
+                        </div>
+                      )}
+                    </Draggable>
+                  );
+                })}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
       </div>
     </section>
   );
